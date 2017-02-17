ac604a7fb2d217135e02262501671cc076c245a5
Federico Bergamo <f.bergamo@cineca.it>
2017-02-17 12:22:04 +0100

    base app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit{
+
+  public flexProp:FlexProperties;
+
+  ngOnInit() {
+   this.flexProp = {
+     display : 'flex',
+     flexDirections : 'row',
+     alignItems : 'center'
+   }
+  }
+
+
+
+}
+
+
+export interface FlexProperties{
+  display:string;
+  flexDirections:string;
+  alignItems:string;
 }
